1b3f36979df31def60a974e2970bb4472e14a076
junwoo <bjw9415@gmail.com>
2026-03-19 14:39:31 +0900

    feat: Add Skeleton component

---
+++ src/components/Skeleton.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { cn } from "../shared/lib/utils";
+
+export type SkeletonProps = React.HTMLAttributes<HTMLDivElement>;
+
+export default function Skeleton({ className, ...props }: SkeletonProps) {
+  return (
+    <div
+      className={cn("animate-pulse rounded-md bg-gray-800/80", className)}
+      {...props}
+    />
+  );
+}
